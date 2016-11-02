68851436557d31a5d2f07cbee046a517d167fe88
Cary Stanley <cary@yahoo-inc.com>
2016-11-02 13:27:02 -0700

    Initial Hacks

---
+++ README.md
@@ -0,0 +1,45 @@
+
+- Entities
+  - Glider
+  - Dart
+  - Copter
+  - Balloon
+  - Toast
+  - Ball
+  - Drip
+  - Fish
+  - Cat
+  - MacPlus
+  - TV/Coffee/VCR?
+  - Outlet
+  - Candle
+  - Vent
+  - Clock
+  - Shredder
+  - Block/Wall/Shelf 
+
+- House
+  - Room
+
+- GameTimer
+- BackBuffer
+- DirtyRect?
+- Sound
+- Keyboard
+ 
+https://github.com/softdorothy/glider_pro/blob/master/Sources/Player.c#L64
+#define kGravity				3
+#define kHImpulse				2
+#define kVImpulse				2
+#define kMaxHVel				16
+
+Input.c
+#define kNormalThrust		5
+#define kHyperThrust		8
+
+Interactions.c
+#define kFloorVentLift			-6
+#define kCeilingVentDrop		8
+#define kFanStrength			12
+
+
---
+++ main.js
@@ -0,0 +1,216 @@
+window.onload = function() {
+
+window.requestAnimFrame = (function(){
+    return  window.requestAnimationFrame     ||
+        window.webkitRequestAnimationFrame ||
+        window.mozRequestAnimationFrame    ||
+        function( callback ){
+            window.setTimeout(callback, 1000 / 60);
+        };
+})();
+
+function loadImage(url, options) {
+    options = options || {};
+    var image = new Image();
+    if (options.onload)
+        image.on('load', options.onload);
+    image.src = url;
+    return image;
+}
+
+function render() {
+    var width = canvas.width;
+    var height = canvas.height;
+
+    backBufferContext.fillStyle="white";
+    backBufferContext.fillRect(0, 0, width, height);
+    glider.render(backBufferContext);
+    ball.render(backBufferContext);
+    context.drawImage(backBuffer, 0, 0, width, height);
+}
+
+function update() {
+    glider.update();
+    ball.update();
+    if (Rect.overlap(glider.body, ball.body)) {
+        glider.die();
+    }
+}
+
+var GROUND = 300;
+
+var canvas = document.getElementById('mainCanvas');
+var context = canvas.getContext('2d');
+var backBuffer = document.createElement("canvas");
+backBuffer.width = canvas.width;
+backBuffer.height = canvas.height;
+var backBufferContext = backBuffer.getContext("2d");
+// var enemy = loadImage('./enemy.gif');
+// var pattern = loadImage('./pattern.png');
+var stats = new Stats();
+
+stats.setMode(0);
+stats.domElement.style.position = 'absolute';
+stats.domElement.style.right = '0px';
+stats.domElement.style.top = '0px';
+document.body.appendChild(stats.domElement);
+KeyboardInput.Initialize();
+
+var Rect = {
+    overlap: function(a, b) {
+        var al = a.x; var bl = b.x; // left
+        var ar = al + a.w; var br = bl + b.w; // right
+        var at = a.y; var bt = b.y; // top
+        var ab = at + a.h; var bb = bt + b.h; // bottom
+
+        if (ab < bt) return false;
+        if (at > bb) return false;
+        if (ar < bl) return false;
+        if (al > br) return false;
+
+        return true;
+    } 
+};
+
+var glider = {
+    lives: 3,
+    update: function () {
+        var body = this.body;
+
+        body.y += 3;
+        var dx = 0;
+        if (KeyboardInput.IsKeyDown(Keys.Left)) {
+            dx = -2;
+        } else if (KeyboardInput.IsKeyDown(Keys.Right)) {
+            dx = 2;
+        }
+        body.x += dx;
+        if (body.y > GROUND) {
+            this.die();
+        }
+    },
+    die: function() {
+        var body = this.body;
+        if (this.lives > 0) {
+            this.lives--;
+            body.y = 0;
+            body.x = 0;
+        }
+    },
+    render: function (ctx) {
+        var body = this.body;
+        ctx.strokeRect(body.x, body.y, body.w, body.h);
+    },
+    body: {
+        w: 50,
+        h: 30,
+        x: 0,
+        y: 0
+    }
+}
+
+var ball = {
+    initialV: -5,
+    initialY: GROUND-30,
+    v: -5,
+    gravity: 0.1,
+    update: function () {
+        var body = this.body;
+
+        body.y += this.v;
+        this.v += this.gravity;
+
+        if (body.y > this.initialY) {
+            body.y = this.initialY;
+            this.v = this.initialV;
+        }
+    },
+    render: function (ctx) {
+        var body = this.body;
+        ctx.strokeRect(body.x, body.y, body.w, body.h);
+    },
+    body: {
+        w: 30,
+        h: 30,
+        x: 100,
+        y: GROUND-30
+    }
+};
+
+function mainloop(){
+    stats.begin();
+    update();
+    render();
+    stats.end();
+    requestAnimFrame(mainloop);
+};
+
+mainloop();
+
+};
+
+var Keys = {
+    A: 65,
+    B: 66,
+    C: 67,
+    D: 68,
+    E: 69,
+    F: 70,
+    G: 71,
+    H: 72,
+    I: 73,
+    J: 74,
+    K: 75,
+    L: 76,
+    M: 77,
+    N: 78,
+    O: 79,
+    P: 80,
+    Q: 81,
+    R: 82,
+    S: 83,
+    T: 84,
+    U: 85,
+    V: 86,
+    W: 87,
+    X: 88,
+    Y: 89,
+    Z: 80,
+    Left: 37,
+    Up: 38,
+    Right: 39,
+    Down: 40
+};
+
+var KeyboardInput = {
+    Pressed: new Array(),
+    
+    Initialize: function() {
+        var self = this;
+        document.onkeydown = function(event) { self.KeyDownEvent(event); }
+        document.onkeyup = function(event) { self.KeyUpEvent(event); }
+    },
+    
+    IsKeyDown: function(key) {
+        if (this.Pressed[key] != null)
+            return this.Pressed[key];
+        return false;
+    },
+    
+    KeyDownEvent: function(event) {
+        this.Pressed[event.keyCode] = true;
+        this.PreventScrolling(event);
+    },
+    
+    KeyUpEvent: function(event) {
+        this.Pressed[event.keyCode] = false;
+        this.PreventScrolling(event);
+    },
+
+    PreventScrolling: function(event) {
+        // 37: left, 38: up, 39: right, 40: down
+        if(event.keyCode >= 37 && event.keyCode <= 40){
+            event.preventDefault();
+        }
+    }
+};
