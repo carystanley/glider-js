d885b027aad2ebe72aff94c5e29476ce1319cc6d
Cary Stanley <cary@yahoo-inc.com>
2016-12-06 14:02:48 -0800

    Add Additional Constants

--- src/const.js
+++ src/const.js
@@ -2,10 +2,23 @@ let OriginalFramerate = 30;
 
 export default {
     OriginalFramerate: OriginalFramerate,
+
     Gravity: 3 * OriginalFramerate,
+
     HImpulse: 2 * OriginalFramerate,
     VImpulse: 2 * OriginalFramerate,
+
     NormalThrust: 5 * OriginalFramerate,
     MaxHVel: 16 * OriginalFramerate,
-    Ground: 300
+
+    CopterHVel: 1 * OriginalFramerate,
+    CopterVVel: 2 * OriginalFramerate,
+
+    CopterHVel: 0 * OriginalFramerate,
+    CopterVVel: -2 * OriginalFramerate,
+
+    DripGravity: .5 * OriginalFramerate,
+
+    Ground: 310
+    Ceil: 8
 };
