18623457ee4e16e4fb4d77ebb36e6acb7e77c4dc
Cary Stanley <cary@yahoo-inc.com>
2017-06-16 10:02:15 -0700

    lint fixes

--- src/main.js
+++ src/main.js
@@ -16,69 +16,68 @@ import SpriteConfig from './config/sprite.json';
 import Graphics from './utils/graphics';
 
 window.onload = function() {
+    var stats = new Stats();
+    var mainGraphics = new Graphics(document.getElementById('mainCanvas'), loadSprites({
+        "glider.gif": loadImage('glider.gif')
+    }, SpriteConfig));
 
-function render(g) {
-    g.clear();
+    stats.setMode(0);
+    stats.domElement.style.position = 'absolute';
+    stats.domElement.style.right = '0px';
+    stats.domElement.style.top = '0px';
+    document.body.appendChild(stats.domElement);
+    KeyboardInput.Initialize();
 
-    glider.renderShadow(g);
-    Room.entries.forEach(function(entity) {
-        entity.render(g);
-    });
-    glider.render(g);
+    var glider = new Glider();
+    var Room = {
+        entries: [
+            new Ball({bounce: 2.5, x: 100, y: Const.Ground-30}),
+            new Shelf({x: 75, y: 120, w: 150}),
+            new Vent({x: 50, h: 200}),
+            new Copter({x: 200, dir: -1})
+        ]
+    };
 
-    g.push();
-}
+    function render(g) {
+        g.clear();
 
-function update() {
+        glider.renderShadow(g);
+        Room.entries.forEach(function(entity) {
+            entity.render(g);
+        });
+        glider.render(g);
 
-    if (KeyboardInput.IsKeyDown(Keys.Left)) {
-        glider.control(-1);
-    } else if (KeyboardInput.IsKeyDown(Keys.Right)) {
-        glider.control(1);
-    } else {
-        glider.control(0);
+        g.push();
     }
 
-    glider.update();
-    Room.entries.forEach(function(entity) {
-        entity.update();
-        if (Rect.overlap(glider.body, entity.body)) {
-            entity.onCollide(glider);
+    function update() {
+
+        if (KeyboardInput.IsKeyDown(Keys.Left)) {
+            glider.control(-1);
+        } else if (KeyboardInput.IsKeyDown(Keys.Right)) {
+            glider.control(1);
+        } else {
+            glider.control(0);
         }
-    });
-}
 
-var stats = new Stats();
-var mainGraphics = new Graphics(document.getElementById('mainCanvas'), loadSprites({
-    "glider.gif": loadImage('glider.gif')
-}, SpriteConfig));
+        glider.update();
+        Room.entries.forEach(function(entity) {
+            entity.update();
+            if (Rect.overlap(glider.body, entity.body)) {
+                entity.onCollide(glider);
+            }
+        });
+    }
 
-stats.setMode(0);
-stats.domElement.style.position = 'absolute';
-stats.domElement.style.right = '0px';
-stats.domElement.style.top = '0px';
-document.body.appendChild(stats.domElement);
-KeyboardInput.Initialize();
+    var lastTime = Date.now();
+    function mainloop(){
+        stats.begin();
+        update();
+        render(mainGraphics);
+        stats.end();
+        requestAnimFrame(mainloop);
+    };
 
-var glider = new Glider();
-var Room = {
-    entries: [
-        new Ball({bounce: 2.5, x: 100, y: Const.Ground-30}),
-        new Shelf({x: 75, y: 120, w: 150}),
-        new Vent({x: 50, h: 200}),
-        new Copter({x: 200, dir: -1})
-    ]
-};
-
-var lastTime = Date.now();
-function mainloop(){
-    stats.begin();
-    update();
-    render(mainGraphics);
-    stats.end();
-    requestAnimFrame(mainloop);
-};
-
-mainloop();
+    mainloop();
 
 };
